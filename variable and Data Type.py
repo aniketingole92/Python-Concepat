71df3ac5ddf9b21cc7cce666203224e6167ba646
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-06-05 14:42:17 +0530

    Create variable and Data Type.py

---
+++ variable and Data Type.py
@@ -0,0 +1,15 @@
+# Integer
+age = 25
+print(type(age))  # <class 'int'>
+
+# Float
+price = 19.99
+print(type(price))  # <class 'float'>
+
+# String
+name = "Alice"
+print(type(name))  # <class 'str'>
+
+# Boolean
+is_active = True
+print(type(is_active))  # <class 'bool'>
