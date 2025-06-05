3757e5af3b52d434e52ed7cee78b54a7cc243573
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-06-05 14:42:43 +0530

    Create Basic Operator.py

---
+++ Basic Operator.py
@@ -0,0 +1,13 @@
+# Arithmetic
+print(10 + 3)   # 13
+print(10 - 3)   # 7
+print(10 * 3)   # 30
+print(10 / 3)   # 3.333...
+print(10 // 3)  # 3 (floor division)
+print(10 % 3)   # 1 (modulus)
+print(10 ** 3)  # 1000 (exponent)
+
+# Comparison
+print(10 > 3)   # True
+print(10 == 3)  # False
+print(10 != 3)  # True
